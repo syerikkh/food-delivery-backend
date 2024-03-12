3468053c0e4ff7c4014de27b47b5e6f0f8a56acf
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2024-03-12 18:08:11 +0800

    ll

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { signUp, getUsers, logIn, deleteUsers } from "../controllers/authController";
+import { signUp, getUsers, logIn } from "../controllers/authController";
 // import { verifyAccessToken } from "../controllers/verifyToken";
 
 const authRouter = Router();
 
-authRouter.get('/users', getUsers).post('/signUp', signUp).post('/logIn', logIn).delete('/delete', deleteUsers);
-
+authRouter.get('/users', getUsers).post('/signUp', signUp).post('/logIn', logIn);
 export { authRouter };
